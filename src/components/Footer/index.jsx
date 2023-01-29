eb080e01b97139efd420ea47085876aaa9026544
Larissa Olimpio <lrsolimpio@gmail.com>
2023-01-29 14:47:42 -0300

    atualizando o css

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,14 @@
+
 import styles from './Footer.module.css'
 
 function Footer(){
     return(
         <footer className={styles.footer}>
-            <h2>Desenvolvido por Alura</h2>
+           
+            <h2>
+                <a href="https://www.alura.com.br/" target="_blank" rel="noreferrer" >Desenvolvido por Alura</a> e <a href="https://www.linkedin.com/in/larissaolimpio/" target="_blank" rel="noreferrer">Larissa Olimpio</a>
+                </h2>
+           
         </footer>
     )
 }
